0d4900c745b1f8bdbd9f5cb89ee0bc462962ff7a
Ammatarasu <daan.d.devries@student.hu.nl>
2019-02-06 16:00:48 +0100

    tesr

--- Formatieve opdracht 1/Formatief_1_MongoDB.py
+++ Formatieve opdracht 1/Formatief_1_MongoDB.py
@@ -11,18 +11,32 @@ profiles = db['profiles']
 sessions = db['sessions']
 
 def findFirst():
-        pprint.pprint(products.find({}))
+        """Finds first object in the products collection"""
+        object = products.find_one()
+        name = object["name"]
+        return name
 
 
 
-
-def findletter():
+def findletter(letter):
+        """"Finds the first instance of a product with the first letter being letter"""
         for i in products.find():
                 name = i["name"]
-                if name[0] == "R":
-                        print(name)
+                if name[0] == letter:
                         break
+        return name
+def averageprice():
+        calcprice = []
 
-#test
+        for i in products.find():
+                price = i["price"]["selling_price"]
+                calcprice.append(price)
+        average = sum(calcprice)/len(calcprice)
+        europrice = average / 100
+        return europrice
+print(findFirst())
+print(findletter("R"))
+print(averageprice())
+
 
 
