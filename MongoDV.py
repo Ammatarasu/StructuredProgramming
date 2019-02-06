621820cc361af663d0a4735141f07dfae546e797
Ammatarasu <daan.d.devries@student.hu.nl>
2019-02-06 12:37:34 +0100

    testing

---
+++ MongoDV.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import datetime
+import pprint
+client = MongoClient()
+db = client.test_database
+
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+
+
+pprint.pprint(posts.find_one())
+print('github test')
